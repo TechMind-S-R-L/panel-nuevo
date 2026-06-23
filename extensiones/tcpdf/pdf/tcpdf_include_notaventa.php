ff82c59fc3d4cddfbe2b35e11d8356a82a3db0b9
Techmind S.R.L. <163465569+TechMindBo@users.noreply.github.com>
2026-06-23 00:22:51 -0400

    tcpdf_include_notaventa.php

--- extensiones/tcpdf/pdf/tcpdf_include_notaventa.php
+++ extensiones/tcpdf/pdf/tcpdf_include_notaventa.php
@@ -24,24 +24,38 @@
  */
 
 // always load alternative config file for examples
-require_once('config/tcpdf_config_notaventa.php');
+//require_once('config/tcpdf_config_notaventa.php');
 
 // Include the main TCPDF library (search the library on the following directories).
-$tcpdf_include_dirs = array(
-	realpath('../tcpdf.php'),
-	'/usr/share/php/tcpdf/tcpdf.php',
-	'/usr/share/tcpdf/tcpdf.php',
-	'/usr/share/php-tcpdf/tcpdf.php',
-	'/var/www/tcpdf/tcpdf.php',
-	'/var/www/html/tcpdf/tcpdf.php',
-	'/usr/local/apache2/htdocs/tcpdf/tcpdf.php'
-);
-foreach ($tcpdf_include_dirs as $tcpdf_include_path) {
-	if (@file_exists($tcpdf_include_path)) {
-		require_once($tcpdf_include_path);
-		break;
-	}
+//$tcpdf_include_dirs = array(
+	//realpath('../tcpdf.php'),
+	//'/usr/share/php/tcpdf/tcpdf.php',
+	//'/usr/share/tcpdf/tcpdf.php',
+	//'/usr/share/php-tcpdf/tcpdf.php',
+	//'/var/www/tcpdf/tcpdf.php',
+	//'/var/www/html/tcpdf/tcpdf.php',
+	//'/usr/local/apache2/htdocs/tcpdf/tcpdf.php'
+//);
+//foreach ($tcpdf_include_dirs as $tcpdf_include_path) {
+	//if (@file_exists($tcpdf_include_path)) {
+		//require_once($tcpdf_include_path);
+		//break;
+	//}
+//}
+// Rutas absolutas para funcionar igual en Windows y servidores Linux.
+$tcpdf_config_path = __DIR__ . '/config/tcpdf_config_notaventa.php';
+$tcpdf_library_path = dirname(__DIR__) . '/tcpdf.php';
+
+if (!is_file($tcpdf_config_path)) {
+	throw new RuntimeException('No se encontró la configuración TCPDF: '.$tcpdf_config_path);
 }
+if (!is_file($tcpdf_library_path)) {
+	throw new RuntimeException('No se encontró la librería TCPDF: '.$tcpdf_library_path);
+}
+
+require_once $tcpdf_config_path;
+require_once $tcpdf_library_path;
+
 
 //============================================================+
 // END OF FILE
